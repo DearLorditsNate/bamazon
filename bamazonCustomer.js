fd04ab3c925d67d1bb2cd6396957eda09ab98a49
Nate Micinski <nmicinski@gmail.com>
2019-03-08 15:25:54 -0800

    add inquirer prompts, validation for item_id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,15 +29,65 @@ Global Varialbes
 ===============================
 */
 
-connection.connect(function(err) {
-    if (err) throw err;
-    connection.query(
-        "SELECt item_id, product_name, price FROM products",
-        function(err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                console.log("ID: " + res[i].item_id + " || " + "Product: " + res[i].product_name + " || " + "Price: " + res[i].price);
+
+/*
+===============================
+Function Declarations
+===============================
+*/
+
+function initialize() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query(
+            "SELECT item_id, product_name, price, stock_quantity FROM products",
+            function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    console.log("ID: " + res[i].item_id + " || " + "Product: " + res[i].product_name + " || " + "Price: " + res[i].price);
+                }
+                prompt(res);
+            });
+    });
+}
+
+function prompt(res) {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the ID of the item you'd like to purchase.",
+            name: "item_id",
+            validate: function (value) {
+                if (value <= res.length) {
+                    return true;
+                } else {
+                    return "That item ID doesn't exist! Please enter another.";
+                }
             }
+        },
+        {
+            type: "input",
+            message: "How many would you like to buy?",
+            name: "amount"
+        }
+    ]).then(answer => {
+        console.log(answer.item_id);
+        console.log(answer.amount);
+        console.log(res[answer.item_id - 1].stock_quantity);
+        if (answer.amount > res[answer.item_id - 1].stock_quantity) {
+            console.log("Insufficient Quantity! Please place another order.");
+            initialize();
+        } else {
+            console.log("Success! Goodbye.");
             connection.end();
-        });
-})
+        }
+    });
+}
+
+/*
+===============================
+Function Calls
+===============================
+*/
+
+initialize();
